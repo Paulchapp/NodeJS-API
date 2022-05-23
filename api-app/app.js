306b2e424d8b96a6c70bc3c49d60f2ae2bee1b69
Paul Chapdelaine <paulchapdelaine@gmail.com>
2022-05-23 14:03:48 +0200

    Ajout de l'app pour récupérer les arguments

--- api-app/app.js
+++ api-app/app.js
@@ -1,20 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('ici traiter GET')
-})
+app.get('/:arg1/blabla/:arg2', (req, res)=>{
+	console.log(req.params)
+	res.send(req.params)
 
-app.post('/', (req, res) => { 
-	res.send('ici traiter POST')
-})
-
-app.put('/', (req, res) => {
-	res.send('ici traiter PUT')
-})
-
-app.delete('/', (req, res) => {
-	res.send('ici traiter DELETE')
 })
 
 app.listen(3000);
